d4d56735dafa5b855d1dda87ae2e2558f787b9ff
Wahaj-tech <khanwahaj016@gmail.com>
2026-01-19 01:02:01 +0530

    added zustand for state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,13 @@ import SignUpPage from './pages/SignUpPage'
 
 //npm i react-router  for performing routing and in main.jsx wrap the <App/> with BrowserRouter
 //npm i axios
+//npm i zustand (goto store folder ... then,useAuthStore.js)
 
 const App = () => {
   return (
     <div className='min-h-screen bg-slate-900  relative flex items-center justify-center p-4 overflow-hidden'>
       {/* DECORATORS - GRID BG & GLOW SHAPES (these will be in all routes)*/}
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] -z-10" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
       <Routes>
@@ -25,4 +26,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App 
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,22 @@
+//Zustand--->(this is state management library ...like we have to send a variable a=10 from app to component or route page we send it as props a={a} then in that particular route page or component we receive it as props then inside functuon we use it as props.a which is so frustating....insteas of passing it to different components we can create it in a store called zustand store)
+//we can use these states DIRECTLY in any component either children of App or grandChilren 
+
+import { create } from "zustand";
+
+//goto zustand webpage for overview of code ..how to write
+export const useAuthStore=create((set)=>({
+    numberOfUser:0,
+    authUser:{name:"john",age:25,_id:123},
+    isLoggedIn:false,
+    login:()=>{
+        console.log("we just logged in");
+        set({isLoggedIn:true})
+        set((state)=>({numberOfUser:state.numberOfUser+1}))
+    }
+}))
+//this create function takes a function which return a object
+
+//how to use-->
+//const {authUser,isLoading,login}=useAuthStore(); that's it
+
+//we are centralizing the values and using where ever we want directly
