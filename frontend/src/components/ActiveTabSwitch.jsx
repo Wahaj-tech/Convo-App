50f19d6dfa1373924e22d144bf6f926c2a94b0a8
Wahaj-tech <khanwahaj016@gmail.com>
2026-02-17 12:36:58 +0530

    chat container setup done

--- frontend/src/components/ActiveTabSwitch.jsx
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -7,7 +7,7 @@ function ActiveTabSwitch() {
     <div className="tabs tabs-boxed bg-transparent p-2 m-2">{/*tabs and tabs-boxed coming from daisyUI*/}
       <button
         onClick={() => setActiveTab("chats")}
-        className={`tab ${
+        className={`tab w-1/2 ${
           activeTab === "chats" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"
         }`}
       >
@@ -16,7 +16,7 @@ function ActiveTabSwitch() {
 
       <button
         onClick={() => setActiveTab("contacts")}
-        className={`tab ${
+        className={`tab w-1/2 ${
           activeTab === "contacts" ? "bg-cyan-500/20 text-cyan-400" : "text-slate-400"
         }`}
       >
