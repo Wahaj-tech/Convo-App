e7701cd7d6094bf737bad98f5f0ab8deb719b84a
Wahaj-tech <khanwahaj016@gmail.com>
2026-02-21 12:20:29 +0530

    online-users implemented

--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from 'react'
 import UsersLoadingSkeleton from './UserLoadingSkeleton';
 import NoChatsFound from './NoChatsFound';
 import { useChatStore } from '../store/useChatStore';
+import { useAuthStore } from '../store/useAuthStore';
 
 const ChatsList = () => {
   const {getMyChatPartners,chats,isUsersLoading,setSelectedUser}=useChatStore();
+  const {onlineUsers}=useAuthStore();
 
   useEffect(()=>{
     getMyChatPartners();
@@ -21,7 +23,7 @@ const ChatsList = () => {
           onClick={() => setSelectedUser(chat)}
         >
           <div className="flex items-center gap-3">
-            <div className={'avatar online'}>
+            <div className={`avatar ${onlineUsers.includes(chat._id) ? "online" : "offline"}`}>
               <div className="size-12 rounded-full">
                 <img src={chat.profilePic || "/avatar.png"} alt={chat.fullName} />
               </div>
